e6512475ad57213479ddc684f3b49cdddebaf777
Ella <ella.vankirk@gmail.com>
2021-08-18 17:02:33 -0500

    Second Assignment completed, Added some comments

--- src/components/Expenses/ExpenseRendering.jsx
+++ src/components/Expenses/ExpenseRendering.jsx
@@ -1,31 +1,46 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 import "./ExpenseRendering.css";
+import "./ExpenseFilter.css";
+
+//This component brings in the ExpenseItem component to display all of the expenses. We call the ExpenseItem multiple times and pass in the data to each item.
 
 function ExpenseRendering(props) {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const saveFilterSelectionHandler = (filterYear) => {
+    setFilteredYear(filterYear);
+  };
+
+  
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      ></ExpenseItem>
-  </Card>
+    <div >
+      <Card className="expenses">
+           <ExpensesFilter selected={filteredYear} onSaveFilterSelection={saveFilterSelectionHandler} />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        ></ExpenseItem>
+      </Card>
+    </div>
   );
 }
 
--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -3,10 +3,20 @@ import ExpenseForm from './ExpenseForm';
 
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  //this is a function we created that takes pulls the data up from the expense form. It will, in this case, expect our form data. Since we need to add an ID, we use the spread operator to add the previous data to our object, and add an ID. Then, in the ExpenseForm component, we will add props as an argument to our function, and then in the function that handles the data submitHandler, we call props.onSaveExpenseData(dataVariable) to pass the data into onSaveExpense in this component, which then points to the function below and tells it what to do. props.onAddExpense is for passing data up to the App.js where the same thing will happen.
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+  };
+  
+  //in the ExpenseForm tag, we create a listener that then points to the above function to see the data.
   return (
-    <div className="new-expense">
-      <ExpenseForm />
+    <div className="new-expense"> 
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />  
     </div>
   );
 };
